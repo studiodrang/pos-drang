30afce656edee2b4188a30ec5b204db74b9b9339
masahiro-kato <47019857+masahiro-kato@users.noreply.github.com>
2020-04-02 21:03:09 +0900

    w

--- front/src/App.vue
+++ front/src/App.vue
@@ -8,12 +8,13 @@
       <v-container class="fill-height" fluid>
         <v-row align="center" justify="center">
           <v-col class="text-center">
-            <v-simple-table height="300px">
+            <v-simple-table>
               <template v-slot:default>
                 <thead>
                   <tr>
                     <th>Name</th>
                     <th>Barcode</th>
+                    <th>Price</th>
                     <th><v-btn color="success">新規作成</v-btn></th>
                   </tr>
                 </thead>
@@ -21,6 +22,7 @@
                   <tr v-for="item in items" :key="item.id">
                     <td>{{ item.name }}</td>
                     <td>{{ item.barcode }}</td>
+                    <td>{{ item.price }}</td>
                     <td>
                       <v-btn color="warning">編集</v-btn>
                       <v-btn color="error">削除</v-btn>
@@ -29,6 +31,12 @@
                 </tbody>
               </template>
             </v-simple-table>
+              <v-card>
+                <v-text-field label="名称" v-model="item.name"/>
+                <v-text-field label="バーコード" v-model="item.barcode"/>
+                <v-text-field label="金額" v-model="item.price"/>
+                <button @click="create">作成</button>
+              </v-card>
           </v-col>
         </v-row>
       </v-container>
@@ -41,18 +49,35 @@ export default {
   data(){
     return {
       items:[],
+      item:{}
     }
   },
   created(){
-    var req = new Request("/api/items",{
-        method: "GET",
-        dataType : "json",
-    });
-    fetch(req).then(response=>{
+    this.read()
+  },
+  methods:{
+    read(){
+      fetch(new Request("/api/items",{
+          method: "POST",
+          dataType : "json",
+      })).then(response=>{
+          return response.json();
+      }).then((items)=> {
+        this.items = items
+      })
+    },
+    create(){
+      console.log(this.item)
+      fetch(new Request("/api/items/create",{
+          method: "POST",
+          dataType : "json",
+          body: JSON.stringify(this.item)
+      })).then(response=>{
         return response.json();
-    }).then((items)=> {
-      this.items = items
-    })
+      }).then(()=> {
+        this.read()
+      }) 
+    }
   }
 }
 </script>
--- main.go
+++ main.go
@@ -1,50 +1,125 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
-	"log"
+	"io/ioutil"
 	"net/http"
-	"strconv"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-func ItemsRead(w http.ResponseWriter, r *http.Request) {
-	ids, ok := r.URL.Query()["id"]
-	if !ok || len(ids[0]) < 1 {
-		ids = nil
+type Item struct {
+	Id      int    `json:"id"`
+	Name    string `json:"name"`
+	Barcode string `json:"barcode"`
+	Price   string `json:"price"`
+}
+type Sale struct {
+	Id        int
+	SaleItems []struct {
+		ItemId int
+		Volume int
 	}
-	items := []map[string]interface{}{
-		map[string]interface{}{
-			"id":      1,
-			"name":    "商品1",
-			"barcode": "1234",
-		},
-		map[string]interface{}{
-			"id":      2,
-			"name":    "商品2",
-			"barcode": "1235",
-		},
+	SaleAt time.Time
+}
+
+var DB *sql.DB
+
+func main() {
+	var err error
+	DB, err = sql.Open("mysql", "drang:drang@/posdrang")
+	if err != nil {
+		panic(err.Error())
 	}
-	if ids != nil {
-		res := []map[string]interface{}{}
-		id, _ := strconv.Atoi(ids[0])
-		for _, item := range items {
-			if id == item["id"].(int) {
-				res = append(res, item)
-			}
+	defer DB.Close()
+
+	// GET
+	get := http.NewServeMux()
+	get.Handle("/", http.FileServer(http.Dir("front")))
+
+	// POST
+	post := http.NewServeMux()
+	post.HandleFunc("/api/items", ItemsRead)
+	post.HandleFunc("/api/items/create", ItemsCreate)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "POST":
+			post.ServeHTTP(w, r)
+		default:
+			get.ServeHTTP(w, r)
 		}
-		items = res
+	})
+	http.ListenAndServe(":8082", nil)
+}
+
+func ItemsRead(w http.ResponseWriter, r *http.Request) {
+	rows, err := DB.Query("SELECT id,name,barcode,price FROM items")
+	if err != nil {
+		panic(err.Error())
+	}
+	items := []Item{}
+	for rows.Next() {
+		var item Item
+		err = rows.Scan(&item.Id, &item.Name, &item.Barcode, &item.Price)
+		if err != nil {
+			panic(err.Error())
+		}
+		items = append(items, item)
 	}
 	bytes, err := json.Marshal(items)
 	if err != nil {
-		log.Println(err)
-		return
+		panic(err.Error())
 	}
 	w.Write(bytes)
 }
-
-func main() {
-	http.Handle("/", http.FileServer(http.Dir("front")))
-
-	http.HandleFunc("/api/items", ItemsRead)
-	http.ListenAndServe(":8082", nil)
+func ItemsCreate(w http.ResponseWriter, r *http.Request) {
+	bytes, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	var item Item
+	err = json.Unmarshal(bytes, &item)
+	if err != nil {
+		panic(err.Error())
+	}
+	_, err = DB.Exec("INSERT INTO items(name,barcode,price)VALUES(?,?,?)", item.Name, item.Barcode, item.Price)
+	if err != nil {
+		panic(err.Error())
+	}
+	w.Write([]byte("{}"))
+}
+func ItemsUpdate(w http.ResponseWriter, r *http.Request) {
+	bytes, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	var item Item
+	err = json.Unmarshal(bytes, &item)
+	if err != nil {
+		panic(err.Error())
+	}
+	_, err = DB.Exec("UPDATE items SET name = ?,barcode = ?,price = ? WHERE id = ?", item.Name, item.Barcode, item.Price, item.Id)
+	if err != nil {
+		panic(err.Error())
+	}
+	w.Write([]byte("{}"))
+}
+func ItemsDelete(w http.ResponseWriter, r *http.Request) {
+	bytes, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	var item Item
+	err = json.Unmarshal(bytes, &item)
+	if err != nil {
+		panic(err.Error())
+	}
+	_, err = DB.Exec("DELETE FROM items WHERE id = ?", item.Id)
+	if err != nil {
+		panic(err.Error())
+	}
+	w.Write([]byte("{}"))
 }
